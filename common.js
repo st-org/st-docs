64941d7d4510567dcf902adc9808b64da7836774
DDU <ddu6@protonmail.com>
2021-09-04 02:49:04 +0800

    update

--- common.js
+++ common.js
@@ -2,9 +2,9 @@ export async function a(unit,Compiler){
     const element=document.createElement('a')
     element.target='_blank'
     const {src}=unit.options
-    if(typeof src==='string'&&src.length>0){
+    if(typeof src==='string'){
         const [src0,hash]=src.split('#')
-        element.href='?src='+encodeURIComponent(src0)+'#'+encodeURIComponent(hash)
+        element.href='?src='+encodeURIComponent(src0)+'#'+encodeURIComponent(hash??'')
     }
     element.append(await Compiler.compileInlineSTDN(unit.children))
     return element
