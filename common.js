1afc49f40d7b348557a0499158ed150f7293f209
DDU <ddu6@protonmail.com>
2021-09-04 00:17:36 +0800

    update

--- common.js
+++ common.js
@@ -1,6 +1,11 @@
 export async function a(unit,Compiler){
     const element=document.createElement('a')
     element.target='_blank'
+    const {src}=unit.options
+    if(typeof src==='string'&&src.length>0){
+        const [src0,hash]=src.split('#')
+        element.href='?src='+encodeURIComponent(src0)+'#'+encodeURIComponent(hash)
+    }
     element.append(await Compiler.compileInlineSTDN(unit.children))
     return element
 }
---
+++ gen.js
@@ -0,0 +1,17 @@
+const {join}=require('path')
+const {readFileSync,writeFileSync}=require('fs')
+const array=[
+    '{id docs, orbit heading, h2 [Docs]}',
+    '{dl [',
+]
+for(const doc of readFileSync(join(__dirname,'docs'),{encoding:'utf8'}).trim().split('\n')){
+    const string=readFileSync(join(__dirname,doc+'.stdn'),{encoding:'utf8'})
+    const title=string.match(/title \[(.+?)\]/)[1]
+    array.push(`    {dt [{src ${doc}.stdn, a [${title}]}]}`,'    {dd [')
+    for(const [,id,name] of string.matchAll(/id ([a-z-]+).+h2 \[(.+?)\]/g)){
+        array.push(`        {src ${doc}.stdn#${id}, a [${name}]}`)
+    }
+    array.push('    ]}')
+}
+array.push(']}','[]')
+writeFileSync(join(__dirname,'docs.stdn'),array.join('\n'))
