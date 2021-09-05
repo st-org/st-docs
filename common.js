b99d97f16a229cd062dc7681c8310ac8006f92b1
DDU <ddu6@protonmail.com>
2021-09-05 20:20:19 +0800

    update

--- common.js
+++ common.js
@@ -1,13 +1,76 @@
+const supportedCodeExts=[
+    'css',
+    'html',
+    'js',
+    'json',
+    'md',
+    'stdn',
+    'ston',
+    'ts',
+    'txt',
+    'urls',
+]
+const supportedImgExts=[
+    'apng',
+    'avif',
+    'gif',
+    'jpg',
+    'jpeg',
+    'jfif',
+    'pjpeg',
+    'pjp',
+    'png',
+    'svg',
+    'webp',
+]
 export async function a(unit,Compiler){
     const element=document.createElement('a')
-    const {href}=unit.options
-    if(typeof href!=='string'||!href.startsWith('#')){
-        element.target='_blank'
+    let {src,href}=unit.options
+    if(src===true){
+        src=''
+    }else if(typeof src==='number'){
+        src=src.toString()
     }
-    const {src}=unit.options
-    if(typeof src==='string'){
-        const [src0,hash]=src.split('#')
-        element.href='?src='+encodeURIComponent(src0)+'#'+encodeURIComponent(hash??'')
+    if(href===true){
+        href=''
+    }else if(typeof href==='number'){
+        href=href.toString()
+    }
+    if(typeof href==='string'){
+        if(!href.startsWith('#')){
+            element.target='_blank'
+            const url=new URL(href,Compiler.context.dir)
+            if(url.origin.endsWith('.vscode-resource.vscode-webview.net')){
+                const ext=url.pathname.replace(/^.*\./,'')
+                if(supportedCodeExts.includes(ext)){
+                    if(window.openCode){
+                        element.href=''
+                        element.addEventListener('click',e=>{
+                            e.preventDefault()
+                            openCode(url.href,ext)
+                        })
+                    }
+                }else if(supportedImgExts.includes(ext)){
+                    if(window.openImg){
+                        element.href=''
+                        element.addEventListener('click',e=>{
+                            e.preventDefault()
+                            openImg(url.href)
+                        })
+                    }
+                }
+            }
+        }
+    }else if(typeof src==='string'){
+        element.target='_blank'
+        const url=new URL(src,Compiler.context.dir)
+        element.href='?src='+encodeURIComponent(url.href)+url.hash
+        if(window.openSrc){
+            element.addEventListener('click',e=>{
+                e.preventDefault()
+                openSrc(url.href,decodeURIComponent(url.hash).slice(1))
+            })
+        }
     }
     element.append(await Compiler.compileInlineSTDN(unit.children))
     return element
