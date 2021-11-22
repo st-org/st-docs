6d6a3936ac6df6bc32ddde2f21a5338f30c1f7d0
DDU <ddu6@protonmail.com>
2021-11-22 14:47:17 +0800

    update

--- gen.js
+++ gen.js
@@ -4,14 +4,14 @@ function stringToId(string){
     return string.replace(/[^\s\w-]/g,'').toLowerCase().trim().split(/[\s_-]+/).join('-')
 }
 const array=[
-    '{h2 [Docs]}',
+    '{h1 [Docs]}',
     '{dl [',
 ]
 for(const doc of readFileSync(join(__dirname,'docs'),{encoding:'utf8'}).trim().split('\n')){
     const string=readFileSync(join(__dirname,doc+'.stdn'),{encoding:'utf8'})
     const title=string.match(/title \[(.+?)\]/)[1]
     array.push(`    {dt [{src ${doc}.stdn, a [${title}]}]}`,'    {dd [')
-    for(const [,name] of string.matchAll(/(?:^|\n){h2 \[(.+)\]}\n/g)){
+    for(const [,name] of string.matchAll(/(?:^|\n){h1 \[(.+)\]}\n/g)){
         array.push(`        {src ${doc}.stdn#${stringToId(name)}, a [${name}]}`)
     }
     array.push('    ]}')
