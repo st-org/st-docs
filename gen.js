2582470f906ba22e38d58f723a8b980de0cea144
DDU <ddu6@protonmail.com>
2022-01-03 04:12:39 +0800

    update

--- gen.js
+++ gen.js
@@ -1,18 +1,18 @@
-const {join}=require('path')
-const {readFileSync,writeFileSync}=require('fs')
-function stringToId(string){
-    return Array.from(string.slice(0,100).matchAll(/[a-zA-Z0-9]+/g)).join('-').toLowerCase()
+const {join} = require('path')
+const {readFileSync, writeFileSync} = require('fs')
+function stringToId(string) {
+    return Array.from(string.slice(0, 100).matchAll(/[a-zA-Z0-9]+/g)).join('-').toLowerCase()
 }
-const array=[
+const array = [
     '{h1 [Docs]}',
 ]
-for(const doc of readFileSync(join(__dirname,'docs'),{encoding:'utf8'}).trim().split('\n')){
-    const string=readFileSync(join(__dirname,doc+'.stdn'),{encoding:'utf8'})
-    const title=string.match(/title \[(.+?)\]/)[1]
-    array.push(`{dt [{src ${doc}.stdn, a [${title}]}]}`,'{dd [')
-    for(const [,name] of string.matchAll(/(?:^|\n){h1 \[(.+)\]}\n/g)){
+for (const doc of readFileSync(join(__dirname, 'docs'), {encoding: 'utf8'}).trim().split('\n')) {
+    const string = readFileSync(join(__dirname, doc + '.stdn'), {encoding: 'utf8'})
+    const title = string.match(/title \[(.+?)\]/)[1]
+    array.push(`{dt [{src ${doc}.stdn, a [${title}]}]}`, '{dd [')
+    for (const [, name] of string.matchAll(/(?:^|\n){h1 \[(.+)\]}\n/g)) {
         array.push(`    {src ${doc}.stdn#${stringToId(name)}, a [${name}]}`)
     }
-    array.push(']}','[]')
+    array.push(']}', '[]')
 }
-writeFileSync(join(__dirname,'docs.stdn'),array.join('\n'))
+writeFileSync(join(__dirname, 'docs.stdn'), array.join('\n'))
