3658fcabd5d2eb10cf17609140d31790da64add7
DDU <ddu6@protonmail.com>
2023-01-23 20:20:24 +0800

    update

--- gen.js
+++ gen.js
@@ -4,13 +4,13 @@ function stringToId(string) {
     return Array.from(string.slice(0, 100).matchAll(/[a-zA-Z0-9]+/g)).join('-').toLowerCase()
 }
 const array = [
-    '{ucs cfr@0.2.1, global []}',
+    '{ucs cfr@main, global []}',
     '{h1 [Docs]}'
 ]
 for (const file of readFileSync(join(__dirname, 'docs'), {encoding: 'utf8'}).trim().split('\n')) {
     const string = readFileSync(join(__dirname, `stdn/${file}.stdn`), {encoding: 'utf8'})
     const title = string.match(/title \[(.+?)\]/)[1]
-    array.push(`{dt [{src ${file}.stdn, a [${title}]}]}`, '{dd [')
+    array.push(`{src ${file}.stdn, a [${title}]}`, '{dd [')
     for (const [, name] of string.matchAll(/(?:^|\n){h1 \[(.+)\]}\n/g)) {
         array.push(`    {src ${file}.stdn#${stringToId(name)}, a [${name}]}`)
     }
